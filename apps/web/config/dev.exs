124129b41d30ce9f2e2dcdf21e726cdebf610776
Derek Kraan <derek.kraan@gmail.com>
2017-09-29 10:46:57 +0200

    Remove puts in dev.exs

--- apps/web/config/dev.exs
+++ apps/web/config/dev.exs
@@ -19,7 +19,6 @@ use Mix.Config
 # If desired, both `http:` and `https:` keys can be
 # configured to run both http and https servers on
 # different ports.
-IO.inspect "IAM DEV"
 config :web, Web.Endpoint,
   http: [port: 4000],
   debug_errors: true,
