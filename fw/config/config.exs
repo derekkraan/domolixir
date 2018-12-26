38e9c9728bf181327b437a8658735a4edcab0c14
Derek Kraan <derek.kraan@gmail.com>
2018-12-26 15:40:55 +0100

    Use Jason and get lots via RingLogger

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -29,7 +29,14 @@ config :nerves_firmware_ssh,
   ]
 
 config :web, Web.Endpoint,
-  http: [port: 80],
+  http: [
+    port:
+      if Mix.env() == :prod do
+        80
+      else
+        8080
+      end
+  ],
   url: [host: "localhost", port: 80],
   secret_key_base: "PqJn6VoHDJkByj012wOBG8jt+p9GVRkhXvZpZfb9GdnAGy71G+ORBoOyluamRl/3",
   root: Path.dirname(__DIR__),
--- web/web/controllers/event_log_controller.ex
+++ web/web/controllers/event_log_controller.ex
@@ -3,8 +3,18 @@ defmodule Web.EventLogController do
 
   def log(conn, params) do
     logs =
-      RingLogger.next()
-      |> Jason.encode!()
+      RingLogger.get()
+      |> Enum.map(fn {level, {_, msg, ts, mdata}} ->
+        Logger.Formatter.format(
+          Logger.Formatter.compile("[$level] $message"),
+          level,
+          msg,
+          ts,
+          mdata
+        )
+      end)
+      |> Enum.map(&IO.chardata_to_string/1)
+      |> Enum.join("\n")
 
     text(conn, logs)
   end
