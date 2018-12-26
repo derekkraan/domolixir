650e9bbdde6a4c246fe24a8fcadd2b8ad47846b1
Derek Kraan <derek.kraan@gmail.com>
2018-12-26 15:08:20 +0100

    Add plug_cowboy (now required)

--- web/mix.exs
+++ web/mix.exs
@@ -10,9 +10,9 @@ defmodule Web.Mixfile do
       deps_path: "deps",
       lockfile: "mix.lock",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -29,20 +29,21 @@ defmodule Web.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:domo, path: "../domo"},
+      {:plug_cowboy, "~> 1.0"},
+      {:domo, path: "../domo"}
     ]
   end
 end
